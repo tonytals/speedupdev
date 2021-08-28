e72d0aafa6741c5eedb29871f5068b2d6523697e
PiterLion <56650537+pedroleonpython@users.noreply.github.com>
2021-08-28 10:46:31 -0300

    tarefa leon

---
+++ frontend/react/src/Primeiro.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Primeiro = props => {
+    return (
+        <div>
+            <h1>{props.titulo}</h1>
+            <hr></hr>
+            <h2>{props.sub}</h2>
+        </div>
+    )
+}
+
+export default Primeiro;
---
+++ frontend/react/src/first.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+{/* # AQUI É IMPORTADO O REACT   */}
+
+const componente_header = props => {
+    return (
+        <div>
+            <h1>{props.titulo}</h1>
+            <hr></hr>
+            <h2>{props.subtitulo}</h2>
+        </div>
+    )
+    
+}    
+
+
+
+export default First
