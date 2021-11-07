d2a8dd2d99e061e634c5fed93ac51b364f8d71fd
Leon <pedroleonpython@gmail.com>
2021-11-07 14:48:35 -0300

    instalação Axios

--- frontend/react/src/DesireList.jsx
+++ frontend/react/src/DesireList.jsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const DesireList = props => {
+
+    const [object_name, SetObjectName] = useState([]);
+
+    const ControleEnvio = (event) => {
+        event.preventDefault();
+        console.log(object_name);
+    }
+
+    const ControleAsMudancas = (event) => {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+        object_name[name] = value;
+        SetObjectName(object_name);
+    }
+
     return (
+
         <div>
             <h1>Lista de Desejos</h1>
-            <hr></hr>
             <h2>Aqui fica a lista de objetos que dejesa guardar. </h2>
+            <hr></hr>
+            <form onSubmit={ControleEnvio}>
+                <label forHtml="object_name">Nome do item:</label><br/>
+                <input type="text" id="object_name" name="object_name" onChange={ControleAsMudancas}/><br/>
+                <label forHtml="object_value">Preço do item:</label><br/>
+                <input type="text" id="object_value" name="object_value" onChange={ControleAsMudancas}/><br/>
+                <input type="submit" value="Adicionar"/>
+                
+            </form>
+            
+            
+
         </div>
+
+        
     )
 }
 
